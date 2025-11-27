5d234f8f8afb76befe2975763761aa14955d0503
itzpraveen <itzpraveen@users.noreply.github.com>
2025-11-27 18:10:40 +0530

    Add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -24,9 +25,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        id="gtm"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T6CJNRBZ');`,
+        }}
+      />
       <body
         className={`${inter.variable} ${robotoMono.variable} antialiased bg-white text-slate-900`}
       >
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T6CJNRBZ" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+          }}
+        />
         {children}
       </body>
     </html>
